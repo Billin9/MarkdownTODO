426fd4a312b2f3520a46b054d0405dd87bdb3396
Billin9 <Billin9@hotmail.com>
2019-05-22 13:07:16 +0800

    重构 TODO list, 使用 Evernote 标签

--- MarkdownTODO.py
+++ MarkdownTODO.py
@@ -1,19 +1,17 @@
 import sublime
 import sublime_plugin
+import re
 
 
 class MarkdownTodoAddOrDeleteCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         region = self.view.line(self.view.sel()[0])  # sel() 返回光标位置. line() 返回光标所在行的 Region, 需要传递光标位置
         text = self.view.substr(region)  # substr() 返回指定 Region 包含的文本
-        if "- [ ] " in text:
-            new_text = text.replace("- [ ] ", "")
-            self.view.replace(edit, region, new_text)  # 替换区域所有内容
-        elif "- [x] " in text:
-            new_text = text.replace("- [x] ", "")
+        new_text, ok = re.subn(r'<en-todo checked=".*"/>', '', text)
+        if ok:
             self.view.replace(edit, region, new_text)  # 替换区域所有内容
         else:
-            new_text = "- [ ] " + text
+            new_text = '<en-todo checked="false"/>' + text
             self.view.replace(edit, region, new_text)
 
 
@@ -21,9 +19,9 @@ class MarkdownTodoSwitchCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         region = self.view.line(self.view.sel()[0])  # sel() 返回光标位置. line() 返回光标所在行的 Region, 需要传递光标位置
         text = self.view.substr(region)  # substr() 返回指定 Region 包含的文本
-        if "- [ ]" in text:
-            new_text = text.replace("- [ ]", "- [x]")
+        if 'checked="false"' in text:
+            new_text = text.replace('checked="false"', 'checked="true"')
             self.view.replace(edit, region, new_text)  # 替换区域所有内容
-        elif "- [x]" in text:
-            new_text = text.replace("- [x]", "- [ ]")
+        elif 'checked="true"' in text:
+            new_text = text.replace('checked="true"', 'checked="false"')
             self.view.replace(edit, region, new_text)
