d2f404c23e5fec03af306ddf4fac739d3653879f
Billin9 <Billin9@hotmail.com>
2019-05-29 12:54:12 +0800

    识别 + 号 和 * 号前缀待办事项, 智能识别缩进

--- MarkdownTODO.py
+++ MarkdownTODO.py
@@ -1,3 +1,4 @@
+import re
 import sublime
 import sublime_plugin
 
@@ -6,24 +7,25 @@ class MarkdownTodoAddOrDeleteCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         region = self.view.line(self.view.sel()[0])  # sel() 返回光标位置. line() 返回光标所在行的 Region, 需要传递光标位置
         text = self.view.substr(region)  # substr() 返回指定 Region 包含的文本
-        if "- [ ] " in text:
-            new_text = text.replace("- [ ] ", "")
-            self.view.replace(edit, region, new_text)  # 替换区域所有内容
-        elif "- [x] " in text:
-            new_text = text.replace("- [x] ", "")
-            self.view.replace(edit, region, new_text)  # 替换区域所有内容
+        new_text, ok = re.subn(r'. \[.\] ', '', text)  # 使用正则替换旧文本
+        if ok:
+            self.view.replace(edit, region, new_text)  # 替换 sublime view 区域展示内容
         else:
-            new_text = "- [ ] " + text
-            self.view.replace(edit, region, new_text)
+            new_text, ok = re.subn(r'^( *)', r'\1- [ ] ', text)
+            if ok:
+                self.view.replace(edit, region, new_text)  # 替换 sublime view 区域展示内容
+            else:
+                new_text = '- [ ] ' + text
+                self.view.replace(edit, region, new_text)
 
 
 class MarkdownTodoSwitchCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         region = self.view.line(self.view.sel()[0])  # sel() 返回光标位置. line() 返回光标所在行的 Region, 需要传递光标位置
         text = self.view.substr(region)  # substr() 返回指定 Region 包含的文本
-        if "- [ ]" in text:
-            new_text = text.replace("- [ ]", "- [x]")
-            self.view.replace(edit, region, new_text)  # 替换区域所有内容
-        elif "- [x]" in text:
-            new_text = text.replace("- [x]", "- [ ]")
+        if " [ ] " in text:
+            new_text = text.replace(" [ ] ", " [x] ")
+            self.view.replace(edit, region, new_text)
+        elif " [x] " in text:
+            new_text = text.replace(" [x] ", " [ ] ")
             self.view.replace(edit, region, new_text)
